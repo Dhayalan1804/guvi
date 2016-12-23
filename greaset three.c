c82d24bc2d615e98d17332b2e73e1e45183213f2
Sri <dhayalans533@gmail.com>
2016-12-23 15:29:29 +0530

    Create greaset three.c

---
+++ greaset three.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int m,a=100,b=50,c=45;
+  printf("enter the integer");
+  scanf("%d",&m);
+  if(a<=b)
+{
+  printf("a is big");
+}
+else if(b<=c)
+{
+  printf("b is big");
+}
+else
+{
+  printf("c is big");
+}
+}
