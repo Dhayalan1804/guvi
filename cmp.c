61248b5c2e36311da01dc1b1130950f71fa6c0ae
Sri <dhayalans533@gmail.com>
2017-06-30 16:15:20 +0530

    Create cmp.c

---
+++ cmp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+  char str[10];
+  printf("Enter the string:\n"0;
+  scanf("%s",&str);
+  if((strcmp(str,"sunday")==0))
+  {
+    printf("\nFalse");
+  }
+  else
+  {
+    printf("\nTrue");
+  }
+ }
+   
+   
